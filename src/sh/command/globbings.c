b3459b18f5b014ae7e11c6216a101a63e8bf1908
Mai Ly Lehoux <Pasteque@localhost.localdomain>
2019-05-17 16:56:39 +0200

    norm ok

--- src/sh/command/globbings.c
+++ src/sh/command/globbings.c
@@ -14,20 +14,15 @@ char **apply_globbings(char **tab_command)
     int i = 0;
 
     globbuf.gl_offs = 0;
-    while (tab_command[i]) {
+    for (; tab_command[i]; i++)
         glob(tab_command[i], (i == 0 ? GLOB_DOOFFS | GLOB_NOCHECK : \
 GLOB_DOOFFS | GLOB_APPEND | GLOB_NOCHECK), NULL, &globbuf);
-        i++;
-    }
     if ((tmp = malloc(sizeof(char *) * \
 (get_size_array(globbuf.gl_pathv) + 1))) == NULL)
         return NULL;
-    i = 0;
-    while (globbuf.gl_pathv[i]) {
+    for (i = 0; globbuf.gl_pathv[i]; i++)
         if ((tmp[i] = my_strdup(globbuf.gl_pathv[i])) == NULL)
             return NULL;
-        i++;
-    }
     tmp[i] = NULL;
     globfree(&globbuf);
     return tmp;
