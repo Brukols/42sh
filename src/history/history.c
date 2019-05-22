ed10f6140b31a3aabacddf82eebd1cdaf00b9e59
Oriane Aumoitte <oriane.aumoitte@epitech.eu>
2019-05-22 10:02:49 +0200

    add history fct

--- src/history/history.c
+++ src/history/history.c
@@ -7,6 +7,12 @@
 
 #include "shell.h"
 
+void print_end_array(char **array)
+{
+    for (int i = 3; array[i] != NULL; i++)
+        my_printf("-%s", array[i]);
+}
+
 int history(t_info *shell, t_command *command)
 {
     char *buffer = NULL;
@@ -15,10 +21,12 @@ int history(t_info *shell, t_command *command)
     if (get_size_array(command->tab_command) != 1)
         return (RETURN_FAILURE);
     lseek(shell->history, 0, SEEK_SET);
-    while ((buffer = get_next_line(shell->history)) == NULL) {
+    while ((buffer = get_next_line(shell->history)) != NULL) {
         if ((array = my_str_to_word_array(buffer, '-')) == NULL)
             return (RETURN_FAILURE);
-        my_printf("\t%s\t%s\t%s\n", array[0], array[1], array[2]);
+        my_printf("\t%s\t%s\t%s", array[0], array[1], array[2]);
+        print_end_array(array);
+        my_printf("\n");
         free_array(array);
         free(buffer);
     }
