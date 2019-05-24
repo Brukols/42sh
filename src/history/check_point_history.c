3f7af650d4ab670e49a7dcb645c4e2c7c553581d
Oriane Aumoitte <oriane.aumoitte@epitech.eu>
2019-05-24 16:57:16 +0200

    add check_point_history

---
+++ src/history/check_point_history.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2018
+** 42sh
+** File description:
+** check_point_history
+*/
+
+#include "shell.h"
+
+void check_point_history(char *command_line)
+{
+    char *needle;
+
+    if (strstr(command_line, "!! ")
+}
