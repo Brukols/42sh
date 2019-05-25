b7fa285157e3d2f8a4c97564d6a3b8dc1e1a705a
Oriane Aumoitte <oriane.aumoitte@epitech.eu>
2019-05-25 18:16:10 +0200

    correction !!

--- src/history/find_point_history.c
+++ src/history/find_point_history.c
@@ -7,34 +7,12 @@
 
 #include "shell.h"
 
-int verif_point_beg(char *command, int i)
-{
-    if (i == 0) {
-        if (command[i] != '!')
-            return (-1);
-        if (command[i + 1] != '!')
-            return (-1);
-        if (command[i + 2] != '\0' && command[i + 2] != '\t'
-            && command[i + 2] != ' ')
-            return (-1);
-        return (0);
-    }
-    return (-1);
-}
-
 int verif_point(char *command, int i)
 {
-    if (i != 0) {
-        if (command[i] != '!')
-            return (-1);
-        if (command[i + 1] != '!')
-            return (-1);
-        if (command[i + 2] != '\0' && command[i + 2] != '\t'
-            && command[i + 2] != ' ')
-            return (-1);
-        if (command[i - 1] != '\t' && command[i - 1] != ' ')
-            return (-1);
-        return (0);
+    if (command[i] == '!') {
+        if (command[i + 1] == '!')
+            return (0);
+        return (-1);
     }
     return (-1);
 }
@@ -42,9 +20,7 @@ int verif_point(char *command, int i)
 int find_point_history(char *command)
 {
     for (int i = 0; command[i] != '\0'; i++) {
-        if (verif_point_beg(command, i) == 0)
-            return (i);
-        else if (verif_point(command, i) == 0)
+        if (verif_point(command, i) == 0)
             return (i);
     }
     return (-1);
