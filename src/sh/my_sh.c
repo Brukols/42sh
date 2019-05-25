444d18322bc6b9052850ed1a805921249008fac7
Oriane Aumoitte <oriane.aumoitte@epitech.eu>
2019-05-25 17:52:59 +0200

    add history !!

--- src/sh/my_sh.c
+++ src/sh/my_sh.c
@@ -12,7 +12,7 @@ int my_sh(t_info *shell)
     while ((shell->command_line = get_next_line(shell->fd_read)) != NULL) {
         if ((shell->env = repair_env(shell->env)) == NULL)
             return (EXIT_FAILURE);
-        //check_point_history(shell->command_line);
+        shell->command_line = check_point_history(shell->command_line);
         add_in_history(shell->command_line);
         if (command_comma(shell) == RETURN_FAILURE)
             return (RETURN_FAILURE);
