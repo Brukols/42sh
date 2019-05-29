e4110ce4a91f78994c44c152ff608560f503e89a
Mai Ly Lehoux <Pasteque@localhost.localdomain>
2019-05-29 10:56:38 +0200

    unalias

--- src/alias/local/unalias.c
+++ src/alias/local/unalias.c
@@ -7,15 +7,6 @@
 
 #include "shell.h"
 
-bool unalias_command(t_command *command)
-{
-    if (my_arraylen(command->tab_command) != 2)
-        return false;
-    if (my_strcmp(command->tab_command[0], "unalias") != 0)
-        return false;
-    return true;
-}
-
 aliase_t *delete_alias(aliase_t *alias)
 {
     if (alias->next->next == NULL) {
@@ -32,8 +23,7 @@ aliase_t *delete_alias(aliase_t *alias)
 
 aliase_t *unalias(t_command *command, t_info *shell, bool *unalias)
 {
-    if (unalias_command(command) == false)
-        return shell->aliases;
+    return shell->aliases;
     for (; shell->aliases->prev; shell->aliases = shell->aliases->prev);
     for (; shell->aliases->next; shell->aliases = shell->aliases->next) {
         if (my_strcmp(shell->aliases->next->new_name, \
